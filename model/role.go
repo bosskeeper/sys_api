e1364540b4d3e4b3d73b4650210665d1179ef608
itnopadol <it@nopadol.com>
2017-06-28 09:20:08 +0700

    Update Model Role Cut AppID

--- model/role.go
+++ model/role.go
@@ -10,7 +10,6 @@ type Role struct {
 	Id int64 `json:"id" db:"Id"`
 	RoleCode string `json:"role_code" db:"RoleCode"`
 	RoleName string `json:"role_name" db:"RoleName"`
-	AppId int `json:"app_id" db:"AppId"`
 	Description string `json:"description" db:"Description"`
 	ActiveStatus int `json:"active_status" db:"ActiveStatus"`
 	CreatorId int `json:"creator_id" db:"CreatorId"`
@@ -58,8 +57,8 @@ func (r *Role) RoleGetById(db *sqlx.DB, access_token string, role_id int64) erro
 func (r *Role) RoleSave(db *sqlx.DB) (role_code string, err error){
 	r.CreateDateTime = time.Now().String()
 	r.ActiveStatus = 1
-	sql := `insert into Role(RoleCode,RoleName,AppId,Description,ActiveStatus,CreatorId,CreateDateTime) values(?,?,?,?,?,?,?)`
-	res, err := db.Exec(sql,r.RoleCode,r.RoleName,r.AppId,r.Description,r.ActiveStatus,r.CreatorId,r.CreateDateTime)
+	sql := `insert into Role(RoleCode,RoleName,Description,ActiveStatus,CreatorId,CreateDateTime) values(?,?,?,?,?,?)`
+	res, err := db.Exec(sql,r.RoleCode,r.RoleName,r.Description,r.ActiveStatus,r.CreatorId,r.CreateDateTime)
 	if err != nil {
 		fmt.Println(err)
 		return "",err
@@ -74,8 +73,8 @@ func (r *Role) RoleSave(db *sqlx.DB) (role_code string, err error){
 
 func (r *Role)RoleUpdate(db *sqlx.DB)(role_code string, err error){
 	r.EditDateTime = time.Now().String()
-	sql := `update Role set RoleCode=?,RoleName=?,AppId=?,Description=?,ActiveStatus=?,EditorId=?,EditDateTime=? where Id=? `
-	res, err := db.Exec(sql,r.RoleCode,r.RoleName,r.AppId,r.Description,r.ActiveStatus,r.EditorId,r.EditDateTime,r.Id)
+	sql := `update Role set RoleCode=?,RoleName=?,Description=?,ActiveStatus=?,EditorId=?,EditDateTime=? where Id=? `
+	res, err := db.Exec(sql,r.RoleCode,r.RoleName,r.Description,r.ActiveStatus,r.EditorId,r.EditDateTime,r.Id)
 
 	fmt.Println("sql = ", sql)
 	if err != nil {
