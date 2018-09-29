53f6b75811d3484d37d4d31137fd7c3bee20e402
itnopadol <it@nopadol.com>
2018-09-29 12:45:37 +0700

    Edit Login 29092018-2

--- model/login.go
+++ model/login.go
@@ -7,65 +7,66 @@ import (
 	"log"
 	"fmt"
 	//"strconv"
-//	"golang.org/x/net/icmp"
+	//	"golang.org/x/net/icmp"
 )
 
 type Login struct {
-	Id 	 int64 `json:"id" db:"Id"`
-	UserCode string `json:"usercode" db:"UserCode"`
-	UserName string `json:"username" db:"UserName"`
-	Password string `json:"password" db:"Password"`
-	UserActiveStatus int64 `json:"usercctivestatus" db:"UserActiveStatus"`
-	RoleId 	 int64 `json:"roleid" db:"RoleId"`
-	RoleCode string `json:"rolecode" db:"RoleCode"`
-	RoleName string `json:"rolename" db:"RoleName"`
-	AppID  int64 `json:"appid" db:"AppID"`
-	AppCode  string `json:"appcode" db:"AppCode"`
-	AppName  string `json:"appname" db:"AppName"`
-	Menus     []*LoginSub `json:"menu"`
+	Id               int64       `json:"id" db:"Id"`
+	UserCode         string      `json:"usercode" db:"UserCode"`
+	UserName         string      `json:"username" db:"UserName"`
+	Password         string      `json:"password" db:"Password"`
+	BranchName       string      `json:"branch_name" db:"BranchName"`
+	PicPath          string      `json:"pic_path" db:"PicPath"`
+	UserActiveStatus int64       `json:"usercctivestatus" db:"UserActiveStatus"`
+	RoleId           int64       `json:"roleid" db:"RoleId"`
+	RoleCode         string      `json:"rolecode" db:"RoleCode"`
+	RoleName         string      `json:"rolename" db:"RoleName"`
+	AppID            int64       `json:"appid" db:"AppID"`
+	AppCode          string      `json:"appcode" db:"AppCode"`
+	AppName          string      `json:"appname" db:"AppName"`
+	Menus            []*LoginSub `json:"menu"`
 }
 
 type LoginSub struct {
-	MenuID int64 `json:"menuid" db:"MenuID"`
-	MenuCode string `json:"menucode" db:"MenuCode"`
-	MenuName string `json:"menuname" db:"MenuName"`
-	PermissionID int64 `json:"permissionid" db:"PermissionID"`
-	IsCreate int64 `json:"is_create" db:"IsCreate"`
-	IsUpdate  int64 `json:"is_update" db:"IsUpdate"`
-	IsRead  int64 `json:"is_read" db:"IsRead"`
-	IsDelete  int64 `json:"is_delete" db:"IsDelete"`
+	MenuID       int64  `json:"menuid" db:"MenuID"`
+	MenuCode     string `json:"menucode" db:"MenuCode"`
+	MenuName     string `json:"menuname" db:"MenuName"`
+	PermissionID int64  `json:"permissionid" db:"PermissionID"`
+	IsCreate     int64  `json:"is_create" db:"IsCreate"`
+	IsUpdate     int64  `json:"is_update" db:"IsUpdate"`
+	IsRead       int64  `json:"is_read" db:"IsRead"`
+	IsDelete     int64  `json:"is_delete" db:"IsDelete"`
 }
 
 var dbl *sqlx.DB
 
-func ConnectDB(dbName string)(db *sqlx.DB,err error){
-	dsn := "root:[ibdkifu88@tcp(nopadol.net:3306)/"+ dbName +"?parseTime=true&charset=utf8&loc=Local"
-	db, err = sqlx.Connect("mysql",dsn)
+func ConnectDB(dbName string) (db *sqlx.DB, err error) {
+	dsn := "root:[ibdkifu88@tcp(nopadol.net:3306)/" + dbName + "?parseTime=true&charset=utf8&loc=Local"
+	db, err = sqlx.Connect("mysql", dsn)
 	if err != nil {
-		return  nil, err
+		return nil, err
 	}
 
 	return db, err
 }
 
-
-func (l *Login) LoginGetByUser(db *sqlx.DB, access_token string,user_code string,password string,appid int64) (err error) {
+func (l *Login) LoginGetByUser(db *sqlx.DB, access_token string, user_code string, password string, appid int64) (err error) {
 	dbl, err = ConnectDB("sys")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	sql := `select a.Id,a.UserCode,a.UserName,a.Password,a.ActiveStatus as UserActiveStatus,c.id as RoleId,c.RoleCode,c.RoleName,b.AppID,d.AppCode,d.AppName`+
-		` from User as a`+
-		` left join UserRole as b on a.Id=b.UserId`+
-		` left join Role as c on b.RoleId=c.Id`+
-		` left join App as d on b.AppId=d.Id`+
+	sql := `select a.Id,a.UserCode,a.UserName,a.Password,a.ActiveStatus as UserActiveStatus,c.id as RoleId,c.RoleCode,c.RoleName,b.AppID,d.AppCode,d.AppName,case when a.BranchId = 1 then 'สำนักงานใหญ่' when a.BranchId = 2 then 'สาขาสันกำแพง' end BranchName,ifnull(a.PicPath,'') as PicPath ` +
+		` from User as a` +
+		` left join UserRole as b on a.Id=b.UserId` +
+		` left join Role as c on b.RoleId=c.Id` +
+		` left join App as d on b.AppId=d.Id` +
 		` where a.UserCode=? and a.Password=? and b.AppID=? limit 1`
 	l.UserCode = user_code
 	l.Password = password
 	l.AppID = appid
-	err = dbl.Get(l,sql,user_code,password,appid)
+	err = dbl.Get(l, sql, user_code, password, appid)
 	log.Println(sql)
-	fmt.Println("sql = ",sql)
+	fmt.Println("sql = ", sql)
 	if err != nil {
 		log.Println("Error ", err.Error())
 	}
@@ -89,15 +90,14 @@ func (l *Login) LoginGetByUser(db *sqlx.DB, access_token string,user_code string
 	//	"  where a.UserName='"+l.UserName+"' and a.Password='"+l.Password+"' and b.AppID="+ strconv.FormatInt(l.AppID, 10)
 
 	fmt.Println("sqlsub = ", sqlsub)
-		err = dbl.Select(&l.Menus,sqlsub,l.UserCode,l.Password,l.AppID)
-		//err = db.Select(&l.menus,sqlsub)
+	err = dbl.Select(&l.Menus, sqlsub, l.UserCode, l.Password, l.AppID)
+	//err = db.Select(&l.menus,sqlsub)
 
-	fmt.Println("Menus = ", l.UserCode,l.Password,l.AppID)
-		if err != nil {
-			log.Println("Error ", err.Error())
-		}
+	fmt.Println("Menus = ", l.UserCode, l.Password, l.AppID)
+	if err != nil {
+		log.Println("Error ", err.Error())
+	}
 	fmt.Println(l)
 
 	return nil
 }
-
--- model/user.go
+++ model/user.go
@@ -7,43 +7,43 @@ import (
 )
 
 type User struct {
-	Id int64 `json:"id" db:"Id"`
-	UserCode string `json:"user_code,omitempty" db:"UserCode"`
-	UserName string `json:"user_name,omitempty" db:"UserName"`
-	Password string `json:"password,omitempty" db:"Password"`
-	Telephone string `json:"telephone,omitempty" db:"Telephone,omitempty"`
-	SaleId int `json:"sale_id,omitempty" db:"SaleId"`
-	SaleCode string `json:"sale_code,omitempty" db:"SaleCode"`
-	BranchId int `json:"branch_id,omitempty" db:"BranchId"`
-	BranchCode string `json:"branch_code,omitempty" db:"BranchCode"`
-	BranchName string `json:"branch_name,omitempty" db:"BranchName"`
-	ProfitcenterId int `json:"profitcenter_id,omitempty" db:"ProfitcenterId"`
+	Id               int64  `json:"id" db:"Id"`
+	UserCode         string `json:"user_code,omitempty" db:"UserCode"`
+	UserName         string `json:"user_name,omitempty" db:"UserName"`
+	Password         string `json:"password,omitempty" db:"Password"`
+	Telephone        string `json:"telephone,omitempty" db:"Telephone,omitempty"`
+	SaleId           int    `json:"sale_id,omitempty" db:"SaleId"`
+	SaleCode         string `json:"sale_code,omitempty" db:"SaleCode"`
+	BranchId         int    `json:"branch_id,omitempty" db:"BranchId"`
+	BranchCode       string `json:"branch_code,omitempty" db:"BranchCode"`
+	BranchName       string `json:"branch_name,omitempty" db:"BranchName"`
+	ProfitcenterId   int    `json:"profitcenter_id,omitempty" db:"ProfitcenterId"`
 	ProfitcenterCode string `json:"profitcenter_code,omitempty" db:"ProfitcenterCode"`
 	ProfitcenterName string `json:"profitcenter_name,omitempty" db:"ProfitcenterName"`
-	DepartmentId int `json:"department_id,omitempty" db:"DepartmentId"`
-	DepartmentCode string `json:"department_code,omitempty" db:"DepartmentCode"`
-	DepartmentName string `json:"department_name,omitempty" db:"DepartmentName"`
-	ExpertId int `json:"expert_id,omitempty" db:"ExpertId"`
-	ExpertCode string `json:"expert_code,omitempty" db:"ExpertCode"`
-	ExpertName string `json:"expert_name,omitempty" db:"ExpertName"`
-	ActiveStatus int `json:"active_status,omitempty" db:"ActiveStatus"`
-	CreatorId int `json:"creator_id,omitempty" db:"CreatorId"`
-	CreateDateTime string `json:"create_date_time,omitempty" db:"CreateDateTime"`
-	EditorId int `json:"editor_id,omitempty" db:"EditorId"`
-	EditDateTime string `json:"edit_date_time,omitempty" db:"EditDateTime"`
+	DepartmentId     int    `json:"department_id,omitempty" db:"DepartmentId"`
+	DepartmentCode   string `json:"department_code,omitempty" db:"DepartmentCode"`
+	DepartmentName   string `json:"department_name,omitempty" db:"DepartmentName"`
+	ExpertId         int    `json:"expert_id,omitempty" db:"ExpertId"`
+	ExpertCode       string `json:"expert_code,omitempty" db:"ExpertCode"`
+	ExpertName       string `json:"expert_name,omitempty" db:"ExpertName"`
+	PicPath          string `json:"pic_path,omitempty" db:"PicPath" `
+	ActiveStatus     int    `json:"active_status,omitempty" db:"ActiveStatus"`
+	CreatorId        int    `json:"creator_id,omitempty" db:"CreatorId"`
+	CreateDateTime   string `json:"create_date_time,omitempty" db:"CreateDateTime"`
+	EditorId         int    `json:"editor_id,omitempty" db:"EditorId"`
+	EditDateTime     string `json:"edit_date_time,omitempty" db:"EditDateTime"`
 }
 
-
-func (u *User)UserGetById(db *sqlx.DB, access_token string, user_id int64) error{
-	sql := `select a.Id,a.UserCode,a.UserName,a.Password,a.Telephone,a.SaleId,a.SaleCode`+
-			` ,a.BranchId,d.BranchCode,d.BranchName,a.ProfitcenterId,b.ProfitcenterCode,b.ProfitcenterName`+
-			` ,a.DepartmentId,c.DepartmentCode,c.DepartmentName,a.ExpertId,e.ExpertCode,e.ExpertName,a.ActiveStatus`+
-			` from User as a left join ProfitcenterMaster as b on a.ProfitcenterId=b.Id`+
-			` left join DepartmentMaster as c on a.DepartmentId=c.Id`+
-			` left join BranchMaster as d on a.BranchId=d.Id`+
-			` left join ExpertMaster as e on a.ExpertId=e.Id where a.id =? order by a.id limit 1`
+func (u *User) UserGetById(db *sqlx.DB, access_token string, user_id int64) error {
+	sql := `select a.Id,a.UserCode,a.UserName,a.Password,a.Telephone,a.SaleId,a.SaleCode,ifnull(a.PicPath,'') as PicPath` +
+		` ,a.BranchId,d.BranchCode,d.BranchName,a.ProfitcenterId,b.ProfitcenterCode,b.ProfitcenterName` +
+		` ,a.DepartmentId,c.DepartmentCode,c.DepartmentName,a.ExpertId,e.ExpertCode,e.ExpertName,a.ActiveStatus` +
+		` from User as a left join ProfitcenterMaster as b on a.ProfitcenterId=b.Id` +
+		` left join DepartmentMaster as c on a.DepartmentId=c.Id` +
+		` left join BranchMaster as d on a.BranchId=d.Id` +
+		` left join ExpertMaster as e on a.ExpertId=e.Id where a.id =? order by a.id limit 1`
 	u.Id = user_id
-	err := db.Get(u,sql,u.Id)
+	err := db.Get(u, sql, u.Id)
 	if err != nil {
 		fmt.Println(err.Error())
 		return err
@@ -52,17 +52,17 @@ func (u *User)UserGetById(db *sqlx.DB, access_token string, user_id int64) error
 	return nil
 }
 
-func (u *User)UserGetByUserCode(db *sqlx.DB, access_token string, user_code string) error{
-	sql := `select a.Id,a.UserCode,a.UserName,a.Password,a.Telephone,a.SaleId,a.SaleCode`+
-		` ,a.BranchId,d.BranchCode,d.BranchName,a.ProfitcenterId,b.ProfitcenterCode,b.ProfitcenterName`+
-		` ,a.DepartmentId,c.DepartmentCode,c.DepartmentName,a.ExpertId,e.ExpertCode,e.ExpertName,a.ActiveStatus`+
-		` from User as a left join ProfitcenterMaster as b on a.ProfitcenterId=b.Id`+
-		` left join DepartmentMaster as c on a.DepartmentId=c.Id`+
-		` left join BranchMaster as d on a.BranchId=d.Id`+
+func (u *User) UserGetByUserCode(db *sqlx.DB, access_token string, user_code string) error {
+	sql := `select a.Id,a.UserCode,a.UserName,a.Password,a.Telephone,a.SaleId,a.SaleCode` +
+		` ,a.BranchId,d.BranchCode,d.BranchName,a.ProfitcenterId,b.ProfitcenterCode,b.ProfitcenterName` +
+		` ,a.DepartmentId,c.DepartmentCode,c.DepartmentName,a.ExpertId,e.ExpertCode,e.ExpertName,a.ActiveStatus` +
+		` from User as a left join ProfitcenterMaster as b on a.ProfitcenterId=b.Id` +
+		` left join DepartmentMaster as c on a.DepartmentId=c.Id` +
+		` left join BranchMaster as d on a.BranchId=d.Id` +
 		` left join ExpertMaster as e on a.ExpertId=e.Id where a.UserCode =? limit 1`
 	fmt.Println(sql)
 	u.UserCode = user_code
-	err := db.Get(u,sql,u.UserCode)
+	err := db.Get(u, sql, u.UserCode)
 	if err != nil {
 		fmt.Println(err.Error())
 		return err
@@ -70,15 +70,15 @@ func (u *User)UserGetByUserCode(db *sqlx.DB, access_token string, user_code stri
 	return nil
 }
 
-func (u *User)UserGetByKeyword(db *sqlx.DB,access_token string, keyword string) (users []*User,err error){
-	sql := `select a.Id,a.UserCode,a.UserName,a.Password,a.Telephone,a.SaleId,a.SaleCode`+
-		` ,a.BranchId,d.BranchCode,d.BranchName,a.ProfitcenterId,b.ProfitcenterCode,b.ProfitcenterName`+
-		` ,a.DepartmentId,c.DepartmentCode,c.DepartmentName,a.ExpertId,e.ExpertCode,e.ExpertName,a.ActiveStatus`+
-		` from User as a left join ProfitcenterMaster as b on a.ProfitcenterId=b.Id`+
-		` left join DepartmentMaster as c on a.DepartmentId=c.Id`+
-		` left join BranchMaster as d on a.BranchId=d.Id`+
+func (u *User) UserGetByKeyword(db *sqlx.DB, access_token string, keyword string) (users []*User, err error) {
+	sql := `select a.Id,a.UserCode,a.UserName,a.Password,a.Telephone,a.SaleId,a.SaleCode` +
+		` ,a.BranchId,d.BranchCode,d.BranchName,a.ProfitcenterId,b.ProfitcenterCode,b.ProfitcenterName` +
+		` ,a.DepartmentId,c.DepartmentCode,c.DepartmentName,a.ExpertId,e.ExpertCode,e.ExpertName,a.ActiveStatus` +
+		` from User as a left join ProfitcenterMaster as b on a.ProfitcenterId=b.Id` +
+		` left join DepartmentMaster as c on a.DepartmentId=c.Id` +
+		` left join BranchMaster as d on a.BranchId=d.Id` +
 		` left join ExpertMaster as e on a.ExpertId=e.Id where a.UserCode like CONCAT("%",?,"%") or a.UserName like CONCAT("%",?,"%") order by a.Id asc`
-	err = db.Select(&users,sql,keyword,keyword)
+	err = db.Select(&users, sql, keyword, keyword)
 	if err != nil {
 		fmt.Println(err.Error())
 		return nil, err
@@ -87,16 +87,15 @@ func (u *User)UserGetByKeyword(db *sqlx.DB,access_token string, keyword string) 
 	return users, nil
 }
 
-
-func (u *User)UserGetAll(db *sqlx.DB, access_token string) (users []*User,err error){
-	sql := `select a.Id,a.UserCode,a.UserName,a.Password,a.Telephone,a.SaleId,a.SaleCode`+
-			` ,a.BranchId,d.BranchCode,d.BranchName,a.ProfitcenterId,b.ProfitcenterCode,b.ProfitcenterName`+
-			` ,a.DepartmentId,c.DepartmentCode,c.DepartmentName,a.ExpertId,e.ExpertCode,e.ExpertName,a.ActiveStatus`+
-			` from User as a left join ProfitcenterMaster as b on a.ProfitcenterId=b.Id`+
-			` left join DepartmentMaster as c on a.DepartmentId=c.Id`+
-			` left join BranchMaster as d on a.BranchId=d.Id`+
-			` left join ExpertMaster as e on a.ExpertId=e.Id order by a.Id asc`
-	err = db.Select(&users,sql)
+func (u *User) UserGetAll(db *sqlx.DB, access_token string) (users []*User, err error) {
+	sql := `select a.Id,a.UserCode,a.UserName,a.Password,a.Telephone,a.SaleId,a.SaleCode` +
+		` ,a.BranchId,d.BranchCode,d.BranchName,a.ProfitcenterId,b.ProfitcenterCode,b.ProfitcenterName` +
+		` ,a.DepartmentId,c.DepartmentCode,c.DepartmentName,a.ExpertId,e.ExpertCode,e.ExpertName,a.ActiveStatus` +
+		` from User as a left join ProfitcenterMaster as b on a.ProfitcenterId=b.Id` +
+		` left join DepartmentMaster as c on a.DepartmentId=c.Id` +
+		` left join BranchMaster as d on a.BranchId=d.Id` +
+		` left join ExpertMaster as e on a.ExpertId=e.Id order by a.Id asc`
+	err = db.Select(&users, sql)
 	if err != nil {
 		fmt.Println(err.Error())
 		return nil, err
@@ -104,15 +103,15 @@ func (u *User)UserGetAll(db *sqlx.DB, access_token string) (users []*User,err er
 
 	return users, nil
 }
+
 type dateStruct struct {
 	time.Time
 }
 
-
-func (u *User)UserSave(db *sqlx.DB) (user_code string, err error){
-	fmt.Println("UserCode = ",u.UserCode)
+func (u *User) UserSave(db *sqlx.DB) (user_code string, err error) {
+	fmt.Println("UserCode = ", u.UserCode)
 	err = u.GetUserNotExist(db)
-	fmt.Println("check nil =",err)
+	fmt.Println("check nil =", err)
 	if err == nil {
 		fmt.Println(err)
 		return "", err
@@ -120,7 +119,7 @@ func (u *User)UserSave(db *sqlx.DB) (user_code string, err error){
 
 	u.CreateDateTime = time.Now().String()
 
-	fmt.Println("Date = ",u.CreateDateTime)
+	fmt.Println("Date = ", u.CreateDateTime)
 
 	sql := `Insert into User(UserCode,UserName,Password,Telephone,SaleId,SaleCode,BranchId,ProfitcenterId,DepartmentId,ExpertId,CreatorId,CreateDateTime) Values(?,?,?,?,?,?,?,?,?,?,?,?)`
 	res, err := db.Exec(sql,
@@ -137,23 +136,22 @@ func (u *User)UserSave(db *sqlx.DB) (user_code string, err error){
 		u.CreatorId,
 		u.CreateDateTime)
 	if err != nil {
-		fmt.Println("Error = ",err.Error())
+		fmt.Println("Error = ", err.Error())
 		return "", err
 	}
 
 	id, err := res.LastInsertId()
 	u.Id = id
-	fmt.Println("Last Insert Id = ",id)
+	fmt.Println("Last Insert Id = ", id)
 
 	return u.UserCode, nil
 }
 
+func (u *User) UserUpdate(db *sqlx.DB) (user_code string, err error) {
 
-func (u *User)UserUpdate(db *sqlx.DB)(user_code string, err error){
-
-	fmt.Println("UserID = ",u.Id)
+	fmt.Println("UserID = ", u.Id)
 	err = u.GetUserNotExist(db)
-	fmt.Println("check nil =",err)
+	fmt.Println("check nil =", err)
 	if err != nil {
 		fmt.Println(err)
 		return "", err
@@ -161,27 +159,26 @@ func (u *User)UserUpdate(db *sqlx.DB)(user_code string, err error){
 
 	u.EditDateTime = time.Now().String()
 	sql := `update User set UserCode=?,UserName=?,Password=?,Telephone=?,BranchId=?,ProfitcenterId=?,DepartmentId=?,ExpertId=?,ActiveStatus=?,EditorId=?,EditDateTime=? where id = ?`
-	res, err := db.Exec(sql,u.UserCode,u.UserName,u.Password,u.Telephone,u.BranchId,u.ProfitcenterId,u.DepartmentId,u.ExpertId,u.ActiveStatus,u.EditorId,u.EditDateTime,u.Id)
+	res, err := db.Exec(sql, u.UserCode, u.UserName, u.Password, u.Telephone, u.BranchId, u.ProfitcenterId, u.DepartmentId, u.ExpertId, u.ActiveStatus, u.EditorId, u.EditDateTime, u.Id)
 	if err != nil {
 		fmt.Println(err)
 		return "", err
 	}
 
-	update, err :=res.RowsAffected()
+	update, err := res.RowsAffected()
 	if err != nil {
 		fmt.Println(err)
 		return "", err
 	}
-	fmt.Println("",update)
+	fmt.Println("", update)
 
 	return u.UserCode, nil
 }
 
-
-func (u *User)UserDisable(db *sqlx.DB)(user_code string, err error){
-	fmt.Println("UserCode = ",u.Id)
+func (u *User) UserDisable(db *sqlx.DB) (user_code string, err error) {
+	fmt.Println("UserCode = ", u.Id)
 	err = u.GetUserNotExist(db)
-	fmt.Println("check nil =",err)
+	fmt.Println("check nil =", err)
 	if err != nil {
 		fmt.Println(err)
 		return "", err
@@ -189,18 +186,18 @@ func (u *User)UserDisable(db *sqlx.DB)(user_code string, err error){
 
 	u.EditDateTime = time.Now().String()
 	sql := `update User set UserCode=?,ActiveStatus=?,EditorId=?,EditDateTime=? where id = ?`
-	res, err := db.Exec(sql,u.UserCode,u.ActiveStatus,u.EditorId,u.EditDateTime,u.Id)
+	res, err := db.Exec(sql, u.UserCode, u.ActiveStatus, u.EditorId, u.EditDateTime, u.Id)
 	if err != nil {
 		fmt.Println(err)
 		return "", err
 	}
 
-	update, err :=res.RowsAffected()
+	update, err := res.RowsAffected()
 	if err != nil {
 		fmt.Println(err)
 		return "", err
 	}
-	fmt.Println("",update)
+	fmt.Println("", update)
 
 	return u.UserCode, nil
 }
@@ -208,11 +205,10 @@ func (u *User)UserDisable(db *sqlx.DB)(user_code string, err error){
 func (u *User) GetUserNotExist(db *sqlx.DB) error {
 	sql := `select Id from User where Id = ?`
 	err := db.Get(u, sql, u.Id)
-	fmt.Println("Get User =",u.Id)
+	fmt.Println("Get User =", u.Id)
 	if err != nil {
 		fmt.Println(err)
 		return err
 	}
 	return nil
 }
-
