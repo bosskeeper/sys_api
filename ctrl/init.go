5eba5840c08e7f82472a6cab4931a5b9f2d9e006
bosskeeper <47129030+bosskeeper@users.noreply.github.com>
2019-06-14 09:43:59 +0700

    Update init.go
    db.nopadol.net:3306

--- ctrl/init.go
+++ ctrl/init.go
@@ -16,7 +16,7 @@ func init() {
 }
 
 func ConnectDB(dbName string)(db *sqlx.DB,err error){
-	dsn := "root:[ibdkifu88@tcp(nopadol.net:3306)/"+ dbName +"?parseTime=true&charset=utf8&loc=Local"
+	dsn := "root:[ibdkifu88@tcp(db.nopadol.net:3306)/"+ dbName +"?parseTime=true&charset=utf8&loc=Local"
 	db, err = sqlx.Connect("mysql",dsn)
 	if err != nil {
 		return  nil, err
